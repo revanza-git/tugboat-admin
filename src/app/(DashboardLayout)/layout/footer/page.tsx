8ad854fa8240512437c48941b8d184ad243283c1
nusantararegas <it.supportnr@pertamina.com>
2024-06-03 15:03:52 +0700

    First Commit

---
+++ src/app/(DashboardLayout)/layout/footer/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import Link from "next/link";
+const Footer = () => {
+  return (
+    <Box sx={{ pt: 6, textAlign: "center" }}>
+      <Typography>
+        © {new Date().getFullYear()} All rights reserved by{" "}
+        <Link href="https://www.nusantararegas.com">
+          Nusantara Regas
+        </Link>{" "}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
+    
